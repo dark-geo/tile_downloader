486a43e9ed1422b97ebc6c01294824cd0391b46e
magican <myasoedov.al@digdes.com>
2019-02-22 11:32:39 +0300

    Changed coords for Newfoundland

--- __init__.py
+++ __init__.py
@@ -8,6 +8,7 @@ if __name__ == "__main__":
     #                map_=maps.ThunderforestLandscape)
 
     download_in_gtiff("./tiles/Newfoundland_and_labrador.tiff",
-                      bbox=(47.3760346, -60.1171875, 50.0571388, -52.5036621),
-                      zoom=19,
+                      # bbox=(47.3760346, -60.1171875, 50.0571388, -52.5036621),
+                      bbox=(48.8701349, -56.1346436, 49.6213871, -54.7174072),
+                      zoom=14,
                       map_=maps.ThunderforestLandscape)
